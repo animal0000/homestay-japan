eeaeb65de1178d7248ffbbf1441866fa607fc2a1
animal0000 <animal0000@gmail.com>
2026-07-22 11:09:27 +0900

    Add portal API configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+/*
+ * Google Apps Script 웹 앱 주소를 아래에 붙여 넣으세요.
+ * 예: https://script.google.com/macros/s/AKfycb.../exec
+ * 빈 값이면 포털은 안내 화면과 기존 교육 자료만 표시합니다.
+ */
+window.PortalConfig = {
+  apiUrl: ""
+};
